fe02a80845773ef24a89e9b5028f3e97234771d1
zhaojun <zhaojunlucky@sina.com>
2014-08-22 23:33:14 +0800

    change ui from htmlayout 2 duilib

---
+++ ZPlayer/ZPlayer/main.cpp
@@ -0,0 +1,25 @@
+#include <windows.h>
+#include <tchar.h>
+#pragma once
+#include <UIlib.h>
+using namespace DuiLib;
+
+#pragma comment(lib, "DuiLib.lib")
+
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	virtual LPCTSTR    GetWindowClassName() const   { return _T("DUIMainFrame"); }
+	virtual CDuiString GetSkinFile()                { return _T("../ui/player.xml"); }
+	virtual CDuiString GetSkinFolder()              { return _T(""); }
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+	return 0;
+}
